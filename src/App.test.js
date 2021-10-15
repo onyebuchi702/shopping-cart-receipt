d5e137a20a9dbe159e933018625cae12624976eb
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-15 10:01:47 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import rewire from "rewire"
+import React from "react"
+import ReactDOM from "react-dom"
+const App = rewire("./App")
+const mapStateToProps = App.__get__("mapStateToProps")
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// @ponicode
+describe("mapStateToProps", () => {
+    test("0", () => {
+        let callFunction = () => {
+            mapStateToProps({ promoCode: { value: "Elio" } })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            mapStateToProps({ promoCode: { value: "Dillenberg" } })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            mapStateToProps({ promoCode: { value: "elio@example.com" } })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            mapStateToProps(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
